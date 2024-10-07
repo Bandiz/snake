132c969d20849df7835ab23184aaaa79f82d6c6c
Linas Jakseboga <linas.jakseboga@gmail.com>
2024-10-07 22:15:04 +0200

    added snake movement

--- Main.gd
+++ Main.gd
@@ -1,24 +1,29 @@
 extends Node
 
-var snake: CharacterBody2D;
+var snake: CharacterBody2D
+var direction: Vector2
+var speed: int = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	snake = get_node("Snake")
+	var viewport_size = get_viewport().size
+	snake.position = viewport_size / 2
+	
+	direction = Vector2.DOWN
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#if Input.is_action_pressed("ui_down"):
-		#snake.position += Vector2(0, 20)
-	pass
+	snake.position += direction * speed * delta
 
 func _input(event: InputEvent) -> void:		
 	if event.is_action_pressed("ui_down"):
-		snake.position += Vector2(0, 5)
+		direction = Vector2.DOWN
 	elif event.is_action_pressed("ui_up"):
-		snake.position += Vector2(0, -5)
+		direction = Vector2.UP
 	elif event.is_action_pressed("ui_right"):
-		snake.position += Vector2(5, 0)
+		direction = Vector2.RIGHT
 	elif event.is_action_pressed("ui_left"):
-		snake.position += Vector2(-5, 0)
+		direction = Vector2.LEFT 	
