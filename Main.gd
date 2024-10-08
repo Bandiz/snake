86c295674875a996554e7472e5e7d1903df9513e
Linas Jakseboga <linas.jakseboga@gmail.com>
2024-10-08 22:27:27 +0200

    added walls

--- Main.gd
+++ Main.gd
@@ -9,13 +9,10 @@ func _ready() -> void:
 	snake = get_node("Snake")
 	var viewport_size = get_viewport().size
 	snake.position = viewport_size / 2
-	
 	direction = Vector2.DOWN
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#if Input.is_action_pressed("ui_down"):
 	snake.position += direction * speed * delta
 
 func _input(event: InputEvent) -> void:		
